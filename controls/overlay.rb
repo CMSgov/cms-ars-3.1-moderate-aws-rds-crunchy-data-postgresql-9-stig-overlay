7230c02386a95eee563ea7b475a55cbf8c00abf9
Haynes Jr <dhaynes@mitre.org>
2019-04-18 18:17:02 -0400

    updated overlay and spreadsheet.

---
+++ controls/overlay.rb
@@ -0,0 +1,7 @@
+# encoding: utf-8
+
+include_controls 'cms-ars-3.1-moderate-crunchy-data-postgresql-9-stig-overlay' do
+  # There are currently no changes required for the Crunchy Data PostgreSQL 9 STIG
+  # tailored for CMS ARS 3.1 Moderate to support AWS RDS PostgreSQL 9 for CMS ARS
+  # 3.1 Moderate.
+end
